683872af9c2ac4429429252ecd40db7bae9f012e
Leoul39 <leoulteferi1996@gmail.com>
2026-09-13 21:37:25 +0300

    relocation of files

--- backend/db.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-
-def get_engine(user, password, db, host="localhost", port=5432):
-    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
-    return create_engine(url)
